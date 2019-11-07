9cb20987fefe8502f639bf37d6d5e98e22b73280
Matthew Jones <matts3371@gmail.com>
2019-11-07 19:09:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,6 +19,7 @@ def speak_to_grandma(phrase)
   elseif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
     return "NO, NOT SINCE 1938!"
+    binding.pry
   else
     puts "HUH?! SPEAK UP, SONNY!"
     return "HUH?! SPEAK UP, SONNY!"
