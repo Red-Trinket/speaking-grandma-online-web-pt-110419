e4bdf3639d442d4a7ec56591e623be84ca57707f
Matthew Jones <matts3371@gmail.com>
2019-11-07 19:09:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elseif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-    return "NO, NOT SINCR 1938!"
+    return "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
     return "HUH?! SPEAK UP, SONNY!"
